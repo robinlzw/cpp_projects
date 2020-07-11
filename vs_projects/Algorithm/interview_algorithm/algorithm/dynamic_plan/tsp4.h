3cb2fd434cb656915d5a1211123f3abff0b34985
lizw <849840504@qq.com>
2020-07-12 00:56:33 +0800

    新增

--- vs_projects/Algorithm/interview_algorithm/algorithm/dynamic_plan/tsp4.h
+++ vs_projects/Algorithm/interview_algorithm/algorithm/dynamic_plan/tsp4.h
@@ -21,7 +21,6 @@ int g_all_cnt = 0;  // 计算次数
 std::vector<std::vector<int>> g_saved_cost_2d_vector;
 
 
-
 string getTime()
 {
 	time_t timep;
@@ -31,16 +30,6 @@ string getTime()
 	localtime_s(&nowTime, &timep);
 	strftime(tmp, sizeof(tmp), "%Y-%m-%d-%H:%M:%S", &nowTime);
 	return std::string(tmp);
-}
-
-std::string get_saved_cost_map_key() {
-	std::string key;
-	for (int i = 1; i < N; i++) {
-		if ('0' == g_visited_city[i]) {  // 还未去过的城市
-			key.append(std::to_string(i));
-		}
-	}
-	return key;
 }
 
 int get_saved_cost_j_value() {
@@ -67,58 +56,17 @@ int cost_of_last_city4(int city) {  // 上一个城市为 city 需要的花费
 	for (int i = 1; i < N; i++) {
 		if (g_visited_city[i] == '0') {
 			g_visited_city[i] = '1';
-		
-			//std::string key = get_saved_cost_map_key() + "#" + std::to_string(i);  // 从 还未去过的城市集合 到 i
-			std::string key = g_visited_city + "#" + std::to_string(i);  // 从 还未去过的城市集合 到 i
-#ifdef DEBUG_LOG_ENABLE
-			std::cout << "key = " << key << std::endl;
-#endif
-						
-			if (g_saved_cost_dict.find(key) == g_saved_cost_dict.end()) {  // 时间消耗在 map 的查找上
-				g_saved_cost_dict[key] = cost_of_last_city4(i);
-			}
-			else {
-#ifdef DEBUG_LOG_ENABLE
-				g_repeat_cnt++;
-#endif
-			}
-				
 
-			int new_cost = g_saved_cost_dict[key] + g_costMatrix[i][city];  // 最后去的城市为 i
-			if (cost_to_city > new_cost) {
-				cost_to_city = new_cost;
-			}
-			g_visited_city[i] = '0';
-		}
-	}
-
-	return cost_to_city;
-}
-
-int cost_of_last_city5(int city) {  // 上一个城市为 city 需要的花费
-	g_all_cnt++;
-	int cost_to_city = 1000000000;
-
-	if (g_all_visited_city == g_visited_city) {
-#ifdef DEBUG_LOG_ENABLE
-		std::cout << 0 << " --> " << city << ", cost = " << g_costMatrix[0][city] << std::endl;
-#endif
-		return g_costMatrix[0][city];  // 第一个去的城市		
-	}
-
-	for (int i = 1; i < N; i++) {
-		if (g_visited_city[i] == '0') {
-			g_visited_city[i] = '1';
 
 			if (g_saved_cost_2d_vector[i][get_saved_cost_j_value()] == -1) {
-				g_saved_cost_2d_vector[i][get_saved_cost_j_value()] = cost_of_last_city5(i);
+				g_saved_cost_2d_vector[i][get_saved_cost_j_value()] = cost_of_last_city4(i);
 			}
 			else {
 #ifdef DEBUG_LOG_ENABLE
 				g_repeat_cnt++;
 #endif
 			}
-
+						
 
 			int new_cost = g_saved_cost_2d_vector[i][get_saved_cost_j_value()] + g_costMatrix[i][city];  // 最后去的城市为 i
 			if (cost_to_city > new_cost) {
@@ -130,7 +78,6 @@ int cost_of_last_city5(int city) {  // 上一个城市为 city 需要的花费
 
 	return cost_to_city;
 }
-
 
 int solve() {
 	FILE *stream1;
