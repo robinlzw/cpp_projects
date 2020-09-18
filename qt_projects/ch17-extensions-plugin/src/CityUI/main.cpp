9d78413a5f3001298c6ed58107ff44015d47e989
zhenwei.li <lizhenwei@chinaredstar.cn>
2020-09-18 20:00:36 +0800

    新增: qt 插件项目

---
+++ qt_projects/ch17-extensions-plugin/src/CityUI/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));  
+    engine.addImportPath("./plugin");
+
+
+    return app.exec();
+}
